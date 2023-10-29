2d7b33a2473b826804e5495186dc8440c7333a8a
fernandobarrosd <fdebarros0910-2004@hotmail.com>
2023-10-29 17:28:43 -0300

    Add CrossOrign annotation in UserController.java

--- src/main/java/com/fernando/spring_mvc_github_oauth/controllers/UserController.java
+++ src/main/java/com/fernando/spring_mvc_github_oauth/controllers/UserController.java
@@ -3,10 +3,12 @@ package com.fernando.spring_mvc_github_oauth.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
